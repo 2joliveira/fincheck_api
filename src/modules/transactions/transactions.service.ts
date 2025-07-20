d8fbdc98d1d7b5a2289c4607a6112dfeb64d92dd
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-07-20 14:04:18 -0300

    generate transactions resources and list transactions by user id

---
+++ src/modules/transactions/transactions.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repository';
+
+@Injectable()
+export class TransactionsService {
+  constructor(private readonly transactionRepository: TransactionsRepository) {}
+
+  create(createTransactionDto: CreateTransactionDto) {
+    return 'This action adds a new transaction';
+  }
+
+  findAllByUserId(userId: string) {
+    return this.transactionRepository.findMAny({
+      where: { userId },
+    });
+  }
+
+  update(id: string, updateTransactionDto: UpdateTransactionDto) {
+    return `This action updates a #${id} transaction`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} transaction`;
+  }
+}
