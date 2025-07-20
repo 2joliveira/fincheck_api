122b9fbd62f0b3c45a4d5497a74b7e5e80283bea
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-07-20 14:52:49 -0300

    implement create transaction

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -2,13 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repository';
+import { ValidateBankAccountOwnerShipService } from '../bank-accounts/services/validate-bank-account-ownership.service';
+import { ValidateCategoryOwnerShipService } from '../categories/services/validate-category-ownership.service';
 
 @Injectable()
 export class TransactionsService {
-  constructor(private readonly transactionRepository: TransactionsRepository) {}
+  constructor(
+    private readonly transactionRepository: TransactionsRepository,
+    private readonly validateBankAccountOwnerShipService: ValidateBankAccountOwnerShipService,
+    private readonly validateCategoryOwnerShipService: ValidateCategoryOwnerShipService,
+  ) {}
 
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  async create(userId: string, createTransactionDto: CreateTransactionDto) {
+    const { bankAccountId, categoryId, name, value, type, date } =
+      createTransactionDto;
+
+    await this.validateEntitiesOwnership({ userId, bankAccountId, categoryId });
+
+    return this.transactionRepository.create({
+      data: {
+        userId,
+        bankAccountId,
+        categoryId,
+        name,
+        value,
+        type,
+        date,
+      },
+    });
   }
 
   findAllByUserId(userId: string) {
@@ -18,10 +39,25 @@ export class TransactionsService {
   }
 
   update(id: string, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
+    return { id, updateTransactionDto };
   }
 
   remove(id: string) {
     return `This action removes a #${id} transaction`;
+  }
+
+  private async validateEntitiesOwnership({
+    userId,
+    bankAccountId,
+    categoryId,
+  }: {
+    userId: string;
+    bankAccountId: string;
+    categoryId: string;
+  }) {
+    await Promise.all([
+      this.validateBankAccountOwnerShipService.validate(userId, bankAccountId),
+      this.validateCategoryOwnerShipService.validate(userId, categoryId),
+    ]);
   }
 }
--- src/shared/database/repositories/transactions.repository.ts
+++ src/shared/database/repositories/transactions.repository.ts
@@ -9,4 +9,8 @@ export class TransactionsRepository {
   findMAny(findManyDto: Prisma.TransactionFindManyArgs) {
     return this.prismaService.transaction.findMany(findManyDto);
   }
+
+  create(createDto: Prisma.TransactionCreateArgs) {
+    return this.prismaService.transaction.create(createDto);
+  }
 }
